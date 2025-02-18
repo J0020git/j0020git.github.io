37d995670b806d159cc58064eb9eea322dc0485f
J0020git <ralph.jerome.hernandez@outlook.com>
2025-02-18 23:31:25 +1100

    fix: Fixed jittery animation

--- animateOnScroll.js
+++ animateOnScroll.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries
-      .filter((entry) => entry.isIntersecting)
-      .forEach((entry) => {
-        entry.target.classList.add("viewed");
-        observer.unobserve(entry.target);
-      });
-  });
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries
+        .filter((entry) => entry.isIntersecting)
+        .forEach((entry) => {
+          entry.target.classList.add("viewed");
+          observer.unobserve(entry.target);
+        });
+    },
+    {
+      threshold: 1.0,
+    }
+  );
 
   const allAnimatedElements = document.querySelectorAll(".animate");
 
